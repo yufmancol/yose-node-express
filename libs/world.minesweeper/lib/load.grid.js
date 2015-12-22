44df6f6ff4ef5d0cc320d29ff2b3f1bd523bcee7
Joshua Partogi <proscrumtrainer@live.com>
2015-12-22 16:04:29 +0800

    Pass regression test

--- libs/world.minesweeper/lib/load.grid.js
+++ libs/world.minesweeper/lib/load.grid.js
@@ -1,22 +1,42 @@
 function load(){
 	var grid = [
 				[
-	                ['empty', 'empty', 'bomb' ],
-	                ['empty', 'empty', 'empty'],
-	                ['empty', 'empty', 'empty']
+	                ['empty', 'empty', 'bomb' , 'empty', 'empty', 'bomb', 'empty', 'empty'],
+	                ['empty', 'empty', 'empty', 'bomb',  'empty', 'empty', 'empty', 'bomb'],
+	                ['empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'bomb', 'empty'],
+	                ['empty', 'empty', 'bomb' , 'empty', 'empty', 'bomb', 'empty', 'empty'],
+	                ['empty', 'empty', 'empty', 'bomb',  'empty', 'empty', 'empty', 'bomb'],
+	                ['empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'bomb', 'empty'],
+	                ['empty', 'empty', 'bomb' , 'empty', 'empty', 'bomb', 'empty', 'empty'],
+	                ['empty', 'empty', 'empty', 'bomb',  'empty', 'empty', 'empty', 'bomb']
             	],
             	[
-	            	['bomb' , 'empty', 'empty'],
-	                ['empty', 'empty', 'empty'],
-	                ['empty', 'empty', 'bomb' ]
+	            	['bomb' , 'empty', 'empty', 'bomb' , 'empty', 'empty', 'bomb' , 'empty'],
+	                ['empty', 'empty', 'empty', 'empty', 'empty', 'bomb', 'empty', 'empty'],
+	                ['empty', 'empty', 'bomb' , 'empty', 'empty', 'bomb', 'empty', 'empty'],
+	                ['bomb' , 'empty', 'empty', 'bomb' , 'empty', 'empty', 'bomb' , 'empty'],
+	                ['empty', 'empty', 'empty', 'empty', 'empty', 'bomb', 'empty', 'empty'],
+	                ['empty', 'empty', 'bomb' , 'empty', 'empty', 'bomb', 'empty', 'empty'],
+	                ['bomb' , 'empty', 'empty', 'bomb' , 'empty', 'empty', 'bomb' , 'empty'],
+	                ['empty', 'empty', 'empty', 'empty', 'empty', 'bomb', 'empty', 'empty']
                 ],[
-                	['bomb' , 'bomb' , 'bomb' ],
-	                ['empty', 'empty', 'empty'],
-	                ['empty', 'empty', 'empty']
+                	['bomb' , 'bomb' , 'bomb', 'empty', 'empty', 'empty', 'empty', 'bomb' ],
+	                ['empty', 'empty', 'empty', 'empty', 'bomb' , 'bomb' , 'bomb', 'empty'],
+	                ['empty', 'empty', 'empty', 'bomb' , 'bomb' , 'bomb', 'empty', 'empty'],
+	                ['bomb' , 'bomb' , 'bomb', 'empty', 'empty', 'empty', 'empty', 'bomb' ],
+	                ['empty', 'empty', 'empty', 'empty', 'bomb' , 'bomb' , 'bomb', 'empty'],
+	                ['empty', 'empty', 'empty', 'bomb' , 'bomb' , 'bomb', 'empty', 'empty'],
+	                ['bomb' , 'bomb' , 'bomb', 'empty', 'empty', 'empty', 'empty', 'bomb' ],
+	                ['empty', 'empty', 'empty', 'empty', 'bomb' , 'bomb' , 'bomb', 'empty']
                 ],[
-					['empty', 'empty', 'bomb' ],
-	                ['bomb' , 'empty', 'empty'],
-	                ['empty', 'empty', 'empty']
+					['empty', 'empty', 'bomb' , 'empty', 'empty', 'bomb', 'empty', 'empty'],
+	                ['bomb' , 'empty', 'empty', 'bomb' , 'empty', 'empty', 'bomb' , 'empty'],
+	                ['empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty'],
+	                ['empty', 'empty', 'bomb' , 'empty', 'empty', 'bomb', 'empty', 'empty'],
+	                ['bomb' , 'empty', 'empty', 'bomb' , 'empty', 'empty', 'bomb' , 'empty'],
+	                ['empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty'],
+	                ['empty', 'empty', 'bomb' , 'empty', 'empty', 'bomb', 'empty', 'empty'],
+	                ['bomb' , 'empty', 'empty', 'bomb' , 'empty', 'empty', 'bomb' , 'empty']
                 ]
             ];
 
