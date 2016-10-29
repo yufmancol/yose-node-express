649f7fea3047b1c5f930e72b6f820e9eb325e2ad
Virgiawan Huda Akbar <virgiawan.huda.akbar@gmail.com>
2016-10-29 11:20:37 +0700

    - commit

--- libs/world.astroport/astroport.js
+++ libs/world.astroport/astroport.js
@@ -1,6 +1,6 @@
 var astroport = function(request, response) {
     response.setHeader('Content-Type', 'text/html');
-    response.render('astroport.html',{'name': name});
+    response.render('astroport.html');
 };
 
 module.exports = astroport;
