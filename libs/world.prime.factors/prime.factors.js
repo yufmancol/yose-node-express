5808d9e8f1d5eb2b56aaebb2a83d31c0eec14b4d
Virgiawan Huda Akbar <virgiawan.huda.akbar@gmail.com>
2016-10-29 16:14:40 +0700

    - Handle string

--- libs/world.prime.factors/prime.factors.js
+++ libs/world.prime.factors/prime.factors.js
@@ -1,12 +1,20 @@
 var primeFactorsOf = require('./lib/prime.factors.of');
 
 var primeFactors = function(request, response) {
+  var temp = request.query['number'];
+	var number = parseInt(temp);
 
-	var number = parseInt(request.query['number']);
-  var decomposition = primeFactorsOf(number);
+  var data;
+  if(isNaN(number)){
+    data = { number: temp, error: 'not a number' };
+  }
+  else{
+    var decomposition = primeFactorsOf(number);
+    data = { number: number, decomposition: decomposition };
+  }
 
 	response.setHeader('Content-Type', 'application/json');
-  response.send({ number: number, decomposition: decomposition });
+  response.send(data);
 };
 
 module.exports = primeFactors;
