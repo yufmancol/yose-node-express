48d5f97a86bf85e2342bc1464d69753057690630
Virgiawan Huda Akbar <virgiawan.huda.akbar@gmail.com>
2016-10-29 11:33:37 +0700

    - Refine form. Now can add new ship

--- libs/world.astroport/astroport.js
+++ libs/world.astroport/astroport.js
@@ -1,6 +1,13 @@
 var astroport = function(request, response) {
+    var newShip = "";
+    console.log("luar");
+    if(request.query.ship){
+      console.log(request.query.ship);
+      console.log("dalam");
+      newShip = request.query.ship;
+    }
     response.setHeader('Content-Type', 'text/html');
-    response.render('astroport.html');
+    response.render('astroport.html', {'shipName': newShip});
 };
 
 module.exports = astroport;
