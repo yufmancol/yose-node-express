dc40bfc323524603a7e2c4cfd3899e12276fbdbd
Virgiawan Huda Akbar <virgiawan.huda.akbar@gmail.com>
2016-10-29 15:54:51 +0700

    - Add primeFactors

--- libs/world.prime.factors/lib/prime.factors.of.js
+++ libs/world.prime.factors/lib/prime.factors.of.js
@@ -1,5 +1,9 @@
 primeFactorsOf = function(number) {
 	var factors = [];
+  var iterate = number / 2;
+  for(var i=0;i<iterate;i++){
+    factors.push(2);
+  }
 	return factors;
 };
 
--- libs/world.prime.factors/prime.factors.js
+++ libs/world.prime.factors/prime.factors.js
@@ -1,12 +1,12 @@
 var primeFactorsOf = require('./lib/prime.factors.of');
 
 var primeFactors = function(request, response) {
-    
+
 	var number = parseInt(request.query['number']);
-    var decomposition = primeFactorsOf(number);
+  var decomposition = primeFactorsOf(number);
 
 	response.setHeader('Content-Type', 'application/json');
-    response.send({ number: number, decomposition: decomposition });
+  response.send({ number: number, decomposition: decomposition });
 };
 
 module.exports = primeFactors;
