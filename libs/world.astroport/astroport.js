8063ddb87ece5b8e7c845b84edf0f3c1d6bc012c
Virgiawan Huda Akbar <virgiawan.huda.akbar@gmail.com>
2016-10-29 11:17:19 +0700

    - Add url postfix in astroport

--- libs/world.astroport/astroport.js
+++ libs/world.astroport/astroport.js
@@ -1,5 +1,5 @@
 var astroport = function(request, response) {
-    var name = request.query.name;
+    var name = request.params.name;
     response.setHeader('Content-Type', 'text/html');
     response.render('astroport.html',{'name': name});
 };
